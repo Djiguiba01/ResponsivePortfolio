1f8599319117715a763dce68ed91763e81dea2ed
Djiguiba <djiguibabarema@gmail.com>
2022-12-12 22:15:14 +0100

    mod

---
+++ assets/js/email.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_ccwkq2f";
+    const templateID = "template_2hny5bz";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Message envoyer avec succès")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
+  
